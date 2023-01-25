62645d5e5eeae64c0efe9b0f73a4f13964d212c1
Ryan Aristosa <ryan.aristosa@softvision.com>
2023-01-25 19:41:37 +0800

    Allow access from front-end

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
-INSERT INTO public.navigation (text, icon) VALUES('test', 'test');
+INSERT INTO public.navigation
+    (text, icon)
+VALUES
+    ('home', 'fa-solid fa-house'),
+    ('about', 'fa-solid fa-address-card'),
+    ('experience', 'fa-solid fa-code'),
+    ('project', 'fa-solid fa-diagram-project');
